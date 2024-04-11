8879e32920d0709ab301ada0217e6b0924f5e288
Roma <Gastursolar@gmail.com>
2024-04-11 17:00:47 +0500

    init

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from ru.travelfood.simple_ui import SimpleUtilites as suClass
-import json
-
-
 def on_created(hashMap,_files=None,_data=None):
+    from ru.travelfood.simple_ui import SimpleUtilites as suClass
+    import json
     hashMap.put('CVDetectors', 'barcode')
     hashMap.put('CVSingleDetector', '')
     hashMap.put('CVSkipNested', '')
@@ -15,20 +13,24 @@ def on_created(hashMap,_files=None,_data=None):
         "OnlyNumbers": True
     }
     hashMap.put("SetVisionSettings", json.dumps(settings))
+    suClass.setGlobalHashMap('_object_detector_mode', '[]')
     return hashMap
 
 
 def on_object_detected(hashMap,_files=None,_data=None):
-
+    from ru.travelfood.simple_ui import SimpleUtilites as suClass
+    import json
     current_object = hashMap.get('current_object')
     if not current_object:
         hashMap.put('toast', 'no_object')
     else:
         current_object = json.loads(current_object)
+        print(current_object)
         obj_id = current_object['object_id']
         obj_value = current_object['value']
 
         odm = suClass.getGlobalHashMap('_object_detector_mode')
+        print(odm, type(odm))
         odm = json.loads(odm) if odm else []
         if obj_value == '2762':
             for item in odm:
